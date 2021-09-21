527ddfbd28ffff7f247a6208505be6fadbb6a78f
yuki-meruru <yuki725421@gmail.com>
2021-09-21 14:44:19 +0000

    [add7]bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @users = User.find(params[:id])
-    @books = @user.reverse_order
+    @books = @user.title.page(params[:page]).reverse_order
   end
 
   def index
